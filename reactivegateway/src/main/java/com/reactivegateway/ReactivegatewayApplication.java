b10e5a13098a450bfff3dd920768da261d373c99
hansung1969 <66286831+hansung1969@users.noreply.github.com>
2025-02-13 14:25:58 +0900

    Add files via upload
    gateway

---
+++ reactivegateway/src/main/java/com/reactivegateway/ReactivegatewayApplication.java
@@ -0,0 +1,13 @@
+package com.reactivegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactivegatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactivegatewayApplication.class, args);
+	}
+
+}
---
+++ reactivegateway/src/test/java/com/reactivegateway/ReactivegatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactivegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactivegatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
